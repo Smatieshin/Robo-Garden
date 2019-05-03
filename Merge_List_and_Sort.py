8edbeb123a4099bdfb77a70aa66052f4994b50ec
Smatieshin <50147043+Smatieshin@users.noreply.github.com>
2019-05-03 15:18:29 -0600

    Add files via upload

---
+++ Merge_List_and_Sort.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 14:25:29 2019
+
+@author: Scott
+"""
+#tester = []
+list1 = ['aa', 'xx', 'zz']
+list2 = ['bb', 'cc']
+lists_merge = list1 + list2
+print(list1)
+print(list2)  
+#print(lists_merge(list1, list2))
+print(lists_merge)
+sorted(lists_merge)
+tester = sorted(lists_merge)
+print(sorted(lists_merge))
+print(tester)
